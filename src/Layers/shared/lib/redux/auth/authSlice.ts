6de022fc87599e24a11a5f95efdcb331f0e2f796
Egor Ivankov <egor.ivankov2015@bk.ru>
2023-11-19 00:48:46 +0300

    add types

--- src/Layers/shared/lib/redux/auth/authSlice.ts
+++ src/Layers/shared/lib/redux/auth/authSlice.ts
@@ -1,6 +1,11 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = {
+interface AuthState {
+    isAuth: boolean;
+    isLog: boolean;
+}
+
+const initialState: AuthState = {
     isAuth: false,
     isLog: false
 }
@@ -9,15 +14,16 @@ const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {
-        setAuth: (state, action) => {
-            state.isAuth = action.payload;
-        },
-        setLog: (state, action) => {
-            state.isLog = action.payload;
-        }
+    setAuth: (state, action: PayloadAction<boolean>) => {
+        state.isAuth = action.payload;
+    },
+    setLog: (state, action: PayloadAction<boolean>) => {
+        state.isLog = action.payload;
     }
-})
+}
+});
 
 const { actions, reducer } = authSlice;
 export const { setAuth, setLog } = actions;
 export default reducer;
+
